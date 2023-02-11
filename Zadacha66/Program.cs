edd9a144edadd4f171795827bc4577c72e070c2b
Nadya <Nadi-lv@yandex.ru>
2023-02-11 22:52:44 +0400

    Zadacha66

---
+++ Zadacha66/Program.cs
@@ -0,0 +1,26 @@
+﻿void Sum(int m, int n)
+{
+    Console.Write(SumMN(m - 1, n));
+}
+
+int SumMN(int m, int n)
+{
+    int res = m;
+    if (m == n)
+        return 0;
+    else
+    {
+        m++;
+        res = m + SumMN(m, n);
+        return res;
+    }
+}
+
+
+
+Console.WriteLine("Введите число M ");
+int m = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите число N ");
+int n = Convert.ToInt32(Console.ReadLine());
+Sum(m, n);
