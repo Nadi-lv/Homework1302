b23c820681f06dcfd4dcdd2f0e99c0325cc80c08
Nadya <Nadi-lv@yandex.ru>
2023-02-11 20:24:55 +0400

    Zadacha64

---
+++ Zadacha64/Program.cs
@@ -0,0 +1,17 @@
+﻿
+void Natural(int n, int count)
+{
+    if (count > n)
+    {
+        return;
+    }
+    else
+    {
+        Natural(n, count + 1);
+        Console.Write(count + " ");
+    }
+}
+Console.WriteLine("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+int count = 1;
+Natural(number, count);
